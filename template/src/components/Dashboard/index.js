4cc836ea8e6f0d2a34de88aa9e4067f238814e16
JimmyMcBride <mcbride967@gmail.com>
2019-12-26 15:23:45 -0600

    update

--- template/src/components/Dashboard/index.js
+++ template/src/components/Dashboard/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import { Wrapper, Linkton } from 'bushido-strap';
 
-const Dashboard = () => {
+export default function Dashboard() {
   return (
     <Wrapper>
       <h1>Hello, world!</h1>
       <Linkton to="/counter">Redux Counter</Linkton>
     </Wrapper>
   );
-};
-
-export default HomePage;
+}
